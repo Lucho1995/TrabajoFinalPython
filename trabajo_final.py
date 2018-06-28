e60c9dfa8b1c4472253becadd019d2cb75af5f60
Luciano Puig <luciano.puig95@gmail.com>
2018-06-28 16:21:32 -0300

    fist commit,create main screen

---
+++ trabajo_final.py
@@ -0,0 +1,18 @@
+import pygame,sys
+from pygame.locals import *
+red =(255,0,0)
+pygame.init()
+ventana = pygame.display.set_mode((600,500))
+pygame.display.set_caption("hola")
+#pygame.display.flip()
+#ventana.fill(red)
+imagen = pygame.image.load("imagenes/mano.png").convert()
+posX,posY=50,50
+ventana.blit(imagen,(200,1))
+while True:
+    for evento in pygame.event.get():
+        if evento.type ==QUIT:
+            pygame.quit()
+            sys.exit()
+
+    pygame.display.update()
